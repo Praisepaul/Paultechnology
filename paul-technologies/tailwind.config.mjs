3c5d97864f0c7c6ae06284d1dae1d185e8de90e0
Praise Paul <praise20024christ@gmail.com>
2026-01-17 00:34:36 -0300

    Initial security site build

---
+++ paul-technologies/tailwind.config.mjs
@@ -0,0 +1,15 @@
+export default {
+  darkMode: 'class',
+  content: ['./src/**/*.{astro,html,js,jsx,ts,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        background: '#0b0f14',
+        surface: '#111827',
+        primary: '#38bdf8',
+        danger: '#ef4444'
+      }
+    }
+  },
+  plugins: [],
+}
